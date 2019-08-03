9db4253eac1b892e40ce8c4420627b34a4cf41e6
NIELSPC\Niels <nielsvdam@hotmail.com>
2019-08-04 00:01:23 +0200

    kanon

---
+++ Assets/Scripts/UtilityForce.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UtilityForce : MonoBehaviour
+{
+    public float waitTime = 2;
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player"))
+        {
+            StartCoroutine(EnableAfterSeconds(waitTime));
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if(other.CompareTag("Player"))
+        {
+            GetComponent<AreaEffector2D>().enabled = false;
+        }
+    }
+
+    IEnumerator EnableAfterSeconds(float time)
+    {
+        yield return new WaitForSeconds(time);
+        GetComponent<AreaEffector2D>().enabled = true;
+    }
+}
