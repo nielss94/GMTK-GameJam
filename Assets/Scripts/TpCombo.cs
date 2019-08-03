c546c02742d643cfef377b43db7ded4a9c7f1b1c
NIELSPC\Niels <nielsvdam@hotmail.com>
2019-08-04 00:11:28 +0200

    kanon2

---
+++ Assets/Scripts/TpCombo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TpCombo : MonoBehaviour
+{
+    private Transform destination;
+
+    private void Awake() {
+        destination = transform.GetChild(0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player") || other.gameObject.layer == LayerMask.NameToLayer("Spear"))
+        {
+            other.transform.position = destination.position;
+            other.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        }
+    }
+}
