0936302b98c11ec9dac428ff33cd13d65ab9e991
Bart in t Veld <bartintveld.contact@gmail.com>
2019-08-04 09:58:20 +0200

    Fixed screenshake

---
+++ Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("PlayerDeath"))
+        {
+            Debug.Log("Death");
+        }
+    }
+}
